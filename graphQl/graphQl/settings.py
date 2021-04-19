fae5a266dd831f27b1fdf7567785bcfb5fd752bf
nandaReddy1212 <nandareddy172@gmail.com>
2021-04-19 01:14:36 -0400

    created app links and added TextFiled and migrated

--- graphQl/graphQl/settings.py
+++ graphQl/graphQl/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'links',
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'hackernews.schema.schema',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
